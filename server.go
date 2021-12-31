addc9326ba06a222a068d8e266d8a64432dfd769
Anthony Metcalf <anthony.metcalf1@gmail.com>
2021-12-31 13:53:52 +0000

    Rough out the latest runs table Pt1.

--- server.go
+++ server.go
@@ -3,13 +3,21 @@ package main
 import (
 	"net/http"
 	"text/template"
+	"time"
 )
 
 func RunnerServer(w http.ResponseWriter, r *http.Request) {
+	const shortForm = "2006-Jan-02"
+	date, _ := time.Parse(shortForm, "2013-Feb-03")
+	run := Run{date, 5.42, RunTime{0, 34, 52}}
 	data := struct {
 		PageTitle string
+		Runs      []Run
 	}{
 		PageTitle: "My Latest Runs",
+		Runs: []Run{
+			run,
+		},
 	}
 	t, _ := template.ParseFiles("html\\GetLatest.html")
 
--- server_test.go
+++ server_test.go
@@ -13,10 +13,47 @@ func TestGETRuns(t *testing.T) {
 
 	RunnerServer(response, request)
 
-	got := response.Body.String()
-	want := "Latest Runs"
+	t.Run("Body Contains 'Latest Runs'", func(t *testing.T) {
+		got := response.Body.String()
+		want := "Latest Runs"
 
-	if !strings.Contains(got, want) {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if !strings.Contains(got, want) {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("Returns 200", func(t *testing.T) {
+		got := response.Code
+		want := 200
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+	t.Run("title is 'My Latest Runs'", func(t *testing.T) {
+		got := response.Body.String()
+		want := "<title>My Latest Runs</title>"
+
+		if !strings.Contains(got, want) {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("Contains run table header", func(t *testing.T) {
+		got := response.Body.String()
+		want := "<th>Date</th>\r\n                <th>Distance</th>\r\n                <th>Time</th>\r\n                <th>Pace</th>\r\n"
+
+		if !strings.Contains(got, want) {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("Contains a run", func(t *testing.T) {
+		got := response.Body.String()
+		want := "<td>2013-02-03 00:00:00 +0000 UTC</td>\r\n                <td>5.42km</td>\r\n                <td>{0 34 52}</td>\r\n                <td>6.43</td>\r\n"
+
+		if !strings.Contains(got, want) {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	//TODO GetRunTime should return a properly formatted string.
+
+	//TODO GetRunDate should return 2021-10-31
 }
