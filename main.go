9f1dc73e02f5296431472a29fa8aa8e895468e2e
Anthony Metcalf <anthony.metcalf1@gmail.com>
2022-02-05 20:32:08 +0000

    Seperate in memory store to it's own file

--- main.go
+++ main.go
@@ -6,18 +6,6 @@ import (
 	"os"
 )
 
-type InMemoryRunnerStore struct {
-	runs []Run
-}
-
-func (i *InMemoryRunnerStore) GetRunnerRuns() []Run {
-	return i.runs
-}
-
-func (i *InMemoryRunnerStore) RecordRun(r Run) {
-	i.runs = append(i.runs, r)
-}
-
 func main() {
 	logPath := "development.log"
 
