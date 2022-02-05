8d2f93d24758042cb4262ea2038eac030a8d919c
Anthony Metcalf <anthony.metcalf1@gmail.com>
2022-02-05 14:27:51 +0000

    Storing some random data in memory for now.

--- main.go
+++ main.go
@@ -3,9 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
+type InMemoryRunnerStore struct{}
+
+func (i *InMemoryRunnerStore) GetRunnerRuns() []Run {
+	const shortForm = "2006-Jan-02"
+	date, _ := time.Parse(shortForm, "2013-Feb-03")
+	runs := []Run{
+		{
+			Date:     date,
+			Distance: 5.42,
+			RunTime:  RunTime{0, 34, 52},
+		},
+	}
+	return runs
+}
+
 func main() {
-	handler := &RunnerServer{}
+	handler := &RunnerServer{&InMemoryRunnerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", handler))
 }
