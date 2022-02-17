9681ed2192606543a391a83bb01b15c71cdb4b40
Anthony Metcalf <anthony.metcalf1@gmail.com>
2022-02-17 14:40:59 +0000

    Make static serving the top level to fix favicon

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("html"))))
+	http.Handle("/", http.FileServer(http.Dir("html")))
 
 	http.Handle("/runs", &RunnerServer{&InMemoryRunnerStore{}})
 	log.Fatal(http.ListenAndServe(":5000", nil))
