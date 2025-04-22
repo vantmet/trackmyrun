b1d5d27aff747c42fde7d3897c59987fd558aa86
Anthony Metcalf <anthony.metcalf1@gmail.com>
2025-04-22 21:25:14 +0000

    feat: use the full auth mechnism to get full read access

--- app/tmr-strava/pull_runs.go
+++ app/tmr-strava/pull_runs.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -22,8 +25,15 @@ type StravaActivity struct {
 	ActivityType string    `json:"type"`
 }
 
+type ExchangeToken struct {
+	StravaClientID     string `json:"client_id"`
+	StravaClientSecret string `json:"client_secret"`
+	Code               string `json:"code"`
+	GrantType          string `json:"grant_type"`
+}
+
 // create a new authentication struct
-type Auth struct {
+type RefreshToken struct {
 	StravaClientID     string `json:"client_id"`
 	StravaClientSecret string `json:"client_secret"`
 	RefreshToken       string `json:"refresh_token"`
@@ -41,15 +51,25 @@ type StravaToken struct {
 func main() {
 	// refresh the token
 	var st StravaToken
-	url := "https://www.strava.com/api/v3/oauth/token"
 
 	// load env vars
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Environment not loaded.")
 	}
-	st.AccessToken = os.Getenv("STRAVA_ACCESS_TOKEN")
-	st.ExpiresAt = 1723166586
+	// TODO
+	// 1) Save the token we get after exchange to a file.
+	// 2) Check the file to see if we need to request access using master creds
+	// 3) Check to see if we need to refresh the token,
+	tok := requestAccess()
+	url := "https://www.strava.com/oauth/token"
+	os.Setenv("STRAVA_ACCESS_TOKEN", tok)
+	st, err = exchangeToken(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//st.AccessToken = os.Getenv("STRAVA_ACCESS_TOKEN")
 	if time.Now().Unix() > int64(st.ExpiresAt) {
 		st, err = refreshToken(url)
 	}
@@ -67,9 +87,71 @@ func main() {
 	}
 }
 
+// Initial Access Request
+func requestAccess() (access_tok string) {
+	fmt.Println("Please go to the following URL and Authorizxe the app. Once redirected copy the 'code' and paste it here. Then hit return.")
+	fmt.Printf("https://www.strava.com/oath/authorize?client_id=%s&response_type=code&redirect_uri=http://localhost/ex_tok&approval_prompt=force&scope=read_all,activity:read_all\n: ", os.Getenv("STRAVA_CLIENT_ID"))
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, _ := reader.ReadString('\n')
+
+	return strings.TrimSpace(text)
+}
+
+// create a new function to refresh the token
+func exchangeToken(baseURL string) (st StravaToken, err error) {
+	et := ExchangeToken{
+		StravaClientID:     os.Getenv("STRAVA_CLIENT_ID"),
+		StravaClientSecret: os.Getenv("STRAVA_CLIENT_SECRET"),
+		Code:               os.Getenv("STRAVA_ACCESS_TOKEN"),
+		GrantType:          "authorization_code",
+	}
+	etString, _ := json.Marshal(et)
+
+	// Debug: Print the userAuth struct
+	log.Printf("Exchange Token: %+v", et)
+
+	// create a new http client
+	client := &http.Client{}
+
+	// create a new request
+	req, err := http.NewRequest("POST", baseURL, bytes.NewBuffer(etString))
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	// Add headers and send the request
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error sending request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Debug: Print the response status and body
+	body := new(strings.Builder)
+	io.Copy(body, resp.Body)
+	log.Printf("Response Status: %s", resp.Status)
+	//	log.Printf("Response Body: %s", body)
+
+	if resp.StatusCode != 200 {
+		return st, errors.New("Returned Error: " + body.String())
+	}
+
+	// Handle the response...
+	var tokenResponse StravaToken
+	err = json.Unmarshal([]byte(body.String()), &tokenResponse)
+	if err != nil {
+		log.Fatalf("Error parsing response: %v", err)
+	}
+
+	return tokenResponse, nil
+}
+
 // create a new function to refresh the token
 func refreshToken(baseURL string) (st StravaToken, err error) {
-	userAuth := Auth{
+	userAuth := RefreshToken{
 		StravaClientID:     os.Getenv("STRAVA_CLIENT_ID"),
 		StravaClientSecret: os.Getenv("STRAVA_CLIENT_SECRET"),
 		RefreshToken:       os.Getenv("STRAVA_REFRESH_TOKEN"),
