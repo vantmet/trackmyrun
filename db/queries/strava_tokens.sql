ea6a62388d75891cd0c7653bc22dd72a1c9da6ad
Anthony Metcalf <anthony.metcalf1@gmail.com>
2025-05-28 15:04:38 +0000

    FEAT: Add queries to update strava tokens table

--- db/queries/strava_tokens.sql
+++ db/queries/strava_tokens.sql
@@ -1,2 +1,16 @@
 -- name: GetStravaToken :one
-SELECT * FROm strava_tokens WHERE id=$1;
+SELECT * FROM strava_tokens WHERE id=$1;
+
+-- name: StoreStravaToken :one
+UPDATE strava_tokens SET 
+access_token=$2, expires_at=$3, refresh_token=$4
+WHERE id=$1
+RETURNING *;
+
+-- name: NewStravaToken :one
+INSERT INTO strava_tokens (
+	id, access_token, expires_at, refresh_token
+	) VALUES (
+	$1,$2,$3,$4
+	)
+RETURNING *;
